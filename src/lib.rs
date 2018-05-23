88ad4985efe7f65caaa22dc8ee08f000220de2de
Nikolay Kim <fafhrd91@gmail.com>
2018-05-23 10:46:49 -0700

    add skeptic

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+extern crate http;
+extern crate actix;
+extern crate actix_web;
+extern crate futures;
+extern crate tokio_io;
+extern crate tokio_core;
+
+pub fn test() {
+    println!("test");
+}
