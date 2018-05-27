ede773dd93dfaa5285aac56ce89ef2449b44e933
Armin Ronacher <armin.ronacher@active-4.com>
2018-05-27 10:49:14 -0700

    Outdent examples

--- examples/application/src/main.rs
+++ examples/application/src/main.rs
@@ -6,31 +6,31 @@ mod state;
 
 fn make_app() {
 // <make_app>
-    fn index(req: HttpRequest) -> impl Responder {
-        "Hello world!"
-    }
+fn index(req: HttpRequest) -> impl Responder {
+    "Hello world!"
+}
 
-    let app = App::new()
-        .prefix("/app")
-        .resource("/index.html", |r| r.method(Method::GET).f(index))
-        .finish()
+let app = App::new()
+    .prefix("/app")
+    .resource("/index.html", |r| r.method(Method::GET).f(index))
+    .finish()
 // </make_app>
 ;
 }
 
 fn run_server() {
 // <run_server>
-    let server = server::new(|| {
-        vec![
-            App::new()
-                .prefix("/app1")
-                .resource("/", |r| r.f(|r| HttpResponse::Ok())),
-            App::new()
-                .prefix("/app2")
-                .resource("/", |r| r.f(|r| HttpResponse::Ok())),
-            App::new().resource("/", |r| r.f(|r| HttpResponse::Ok())),
-        ]
-    });
+let server = server::new(|| {
+    vec![
+        App::new()
+            .prefix("/app1")
+            .resource("/", |r| r.f(|r| HttpResponse::Ok())),
+        App::new()
+            .prefix("/app2")
+            .resource("/", |r| r.f(|r| HttpResponse::Ok())),
+        App::new().resource("/", |r| r.f(|r| HttpResponse::Ok())),
+    ]
+});
 // </run_server>
 }
 
