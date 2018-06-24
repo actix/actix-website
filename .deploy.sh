27e11483bca79cfac1a61273e48d86e214d1f4a1
Armin Ronacher <armin.ronacher@active-4.com>
2018-06-24 23:08:27 +0200

    Fixed deploy script

--- .deploy.sh
+++ .deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if [[ "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_BRANCH" == "master" ]]; then
   cp CNAME public
-  mv public/en/* public && rm -R public/en
   git clone https://github.com/davisp/ghp-import.git &&
   ./ghp-import/ghp_import.py -n -p -f -m "Documentation upload" -b master -r https://"$GH_TOKEN"@github.com/actix/actix.github.io.git public &&
   echo "Uploaded documentation"
