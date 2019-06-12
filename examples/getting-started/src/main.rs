a5f7e0d09569e33dda92d88081b3947ee3aa5dcb
Cameron Dershem <cameron@pinkhatbeard.com>
2019-06-12 04:56:59 -0400

    removes outdatej code in examples.

--- examples/getting-started/src/main.rs
+++ examples/getting-started/src/main.rs
@@ -1,20 +1,3 @@
-// // <setup>
-// extern crate actix_web;
-// use actix_web::{server, App, HttpRequest};
-
-// fn index(_req: &HttpRequest) -> &'static str {
-//     "Hello world!"
-// }
-// // </setup>
-// // <main>
-// fn main() {
-//     server::new(|| App::new().resource("/", |r| r.f(index)))
-//         .bind("127.0.0.1:8088")
-//         .unwrap()
-//         .run();
-// }
-// // </main>
-
 // <setup>
 extern crate actix_web;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
@@ -23,6 +6,7 @@ fn index(_req: HttpRequest) -> HttpResponse {
     HttpResponse::Ok().body("Hello world!")
 }
 // </setup>
+
 // <main>
 fn main() {
     HttpServer::new(|| App::new().service(web::resource("/").to(index)))
