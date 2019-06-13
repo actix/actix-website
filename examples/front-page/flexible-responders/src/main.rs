d2a2fc697914fa8e5593c78d0cb80654df82ba04
Cameron Dershem <cameron@pinkhatbeard.com>
2019-06-12 23:25:00 -0400

    Updates front-page flexible-responders to v1.0

--- examples/front-page/flexible-responders/src/main.rs
+++ examples/front-page/flexible-responders/src/main.rs
@@ -1,5 +1,29 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use serde::Serialize;
+
 // <flexible-responders>
+#[derive(Serialize)]
+struct Measurement {
+    temperature: f32,
+}
+
+fn hello_world() -> impl Responder {
+    "Hello World!"
+}
+
+fn current_temperature(_req: HttpRequest) -> impl Responder {
+    web::Json(Measurement { temperature: 42.3 })
+}
+
 fn main() {
-    println!("Hello, world!");
+    HttpServer::new(|| {
+        App::new()
+            .service(web::resource("/").to(hello_world))
+            .service(web::resource("/temp").to(current_temperature))
+    })
+    .bind("127.0.0.1:8000")
+    .expect("Can not bind to port 8000")
+    .run()
+    .unwrap();
 }
 // </flexible-responders>
