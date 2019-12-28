4f2f9303493f619c9956a74cd0970c421333df5a
Yuki Okushi <huyuumi.dev@gmail.com>
2019-12-29 02:32:47 +0900

    Update requests

--- examples/requests/src/main.rs
+++ examples/requests/src/main.rs
@@ -14,15 +14,15 @@ struct Info {
 }
 
 /// extract `Info` using serde
-fn index(info: web::Json<Info>) -> Result<String> {
+async fn index(info: web::Json<Info>) -> Result<String> {
     Ok(format!("Welcome {}!", info.username))
 }
 
-fn main() {
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().route("/", web::post().to(index)))
-        .bind("127.0.0.1:8088")
-        .unwrap()
+        .bind("127.0.0.1:8088")?
         .run()
-        .unwrap();
+        .await
 }
 // </json-request>
