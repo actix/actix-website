1db43f9045d851fdcb3a0f93fce4d33be73c1fcd
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2019-12-28 11:38:43 +0100

    update examples/errors to actix-web 2.0

--- examples/errors/src/recommend_one.rs
+++ examples/errors/src/recommend_one.rs
@@ -1,5 +1,6 @@
 // <recommend-one>
-use actix_web::{error, http, HttpResponse};
+use actix_http::ResponseBuilder;
+use actix_web::{error, http::header, http::StatusCode, HttpResponse};
 use failure::Fail;
 
 #[derive(Fail, Debug)]
@@ -10,26 +11,29 @@ enum UserError {
 
 impl error::ResponseError for UserError {
     fn error_response(&self) -> HttpResponse {
+        ResponseBuilder::new(self.status_code())
+            .set_header(header::CONTENT_TYPE, "text/html; charset=utf-8")
+            .body(self.to_string())
+    }
+    fn status_code(&self) -> StatusCode {
         match *self {
-            UserError::ValidationError { .. } => {
-                HttpResponse::new(http::StatusCode::BAD_REQUEST)
-            }
+            UserError::ValidationError { .. } => StatusCode::BAD_REQUEST,
         }
     }
 }
 // </recommend-one>
-fn index() -> Result<&'static str, UserError> {
+async fn index() -> Result<&'static str, UserError> {
     Err(UserError::ValidationError {
         field: "bad stuff".to_string(),
     })
 }
 
-pub fn main() {
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
     use actix_web::{web, App, HttpServer};
 
     HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind("127.0.0.1:8088")
-        .unwrap()
+        .bind("127.0.0.1:8088")?
         .run()
-        .unwrap();
+        .await
 }
