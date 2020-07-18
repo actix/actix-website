af3c87ccf049bf23ee9f19a215c9482062da1b41
Yuki Okushi <huyuumi.dev@gmail.com>
2020-07-18 15:22:30 +0900

    Remove travis-related files

--- .deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [[ "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_PULL_REQUEST" = "false" && "$TRAVIS_BRANCH" == "master" ]]; then
-  cp CNAME public
-  git clone https://github.com/davisp/ghp-import.git &&
-  ./ghp-import/ghp_import.py -n -p -f -m "Documentation upload" -b master -r https://"$GH_TOKEN"@github.com/actix/actix.github.io.git public &&
-  echo "Uploaded documentation"
-fi
