9b73d380151e59b074b30fb678cefd5a997a9d08
Mark Lodato <mlodato517@gmail.com>
2021-03-31 10:38:17 -0400

    Clarify use of `Arc` in shared state
    **This Commit**
    Adjusts the explanation of shared state in the Extractors page.
    
    **Why?**
    There was some miscommunication in #219 that resulted in a doc change
    being merged without an associated code change. I was about to make
    the code change to match the docs and realized that a better example
    would show how one could only share _part_ of the application state
    using an `Arc`. So, instead of making the Extractor section a
    near-duplicate of the Shared Mutable State section, I tried my best to
    let them both explain different aspects of state-sharing.

--- examples/request-handlers/src/handlers_arc.rs
+++ examples/request-handlers/src/handlers_arc.rs
@@ -1,29 +1,43 @@
 // <arc>
 use actix_web::{get, web, App, HttpServer, Responder};
+use std::cell::Cell;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
 #[derive(Clone)]
 struct AppState {
-    count: Arc<AtomicUsize>,
+    thread_count: Cell<usize>,
+    total_count: Arc<AtomicUsize>,
 }
 
 #[get("/")]
 async fn show_count(data: web::Data<AppState>) -> impl Responder {
-    format!("count: {}", data.count.load(Ordering::Relaxed))
+    format!(
+        "total_count: {}\nthread_count: {}",
+        data.total_count.load(Ordering::Relaxed),
+        data.thread_count.get()
+    )
 }
 
 #[get("/add")]
 async fn add_one(data: web::Data<AppState>) -> impl Responder {
-    data.count.fetch_add(1, Ordering::Relaxed);
+    data.total_count.fetch_add(1, Ordering::Relaxed);
 
-    format!("count: {}", data.count.load(Ordering::Relaxed))
+    let thread_count = data.thread_count.get();
+    data.thread_count.set(thread_count + 1);
+
+    format!(
+        "total_count: {}\nthread_count: {}",
+        data.total_count.load(Ordering::Relaxed),
+        data.thread_count.get()
+    )
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let data = AppState {
-        count: Arc::new(AtomicUsize::new(0)),
+        thread_count: Cell::new(0),
+        total_count: Arc::new(AtomicUsize::new(0)),
     };
 
     HttpServer::new(move || {
