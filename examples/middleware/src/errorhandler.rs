3167a55853e047410c8e74c02e8dfd90e5966dc6
Mark Lodato <mlodato517@gmail.com>
2021-12-22 13:36:35 -0500

    Improve error handler example (#248)

--- examples/middleware/src/errorhandler.rs
+++ examples/middleware/src/errorhandler.rs
@@ -2,9 +2,9 @@
 
 // <error-handler>
 use actix_web::middleware::errhandlers::{ErrorHandlerResponse, ErrorHandlers};
-use actix_web::{dev, http, HttpResponse, Result};
+use actix_web::{dev, http, web, App, HttpResponse, HttpServer, Result};
 
-fn render_500<B>(mut res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
+fn add_error_header<B>(mut res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
     res.response_mut().headers_mut().insert(
         http::header::CONTENT_TYPE,
         http::HeaderValue::from_static("Error"),
@@ -14,18 +14,15 @@ fn render_500<B>(mut res: dev::ServiceResponse<B>) -> Result<ErrorHandlerRespons
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use actix_web::{web, App, HttpServer};
-
     HttpServer::new(|| {
         App::new()
-            .wrap(
-                ErrorHandlers::new()
-                    .handler(http::StatusCode::INTERNAL_SERVER_ERROR, render_500),
-            )
+            .wrap(ErrorHandlers::new().handler(
+                http::StatusCode::INTERNAL_SERVER_ERROR,
+                add_error_header,
+            ))
             .service(
-                web::resource("/test")
-                    .route(web::get().to(|| HttpResponse::Ok()))
-                    .route(web::head().to(|| HttpResponse::MethodNotAllowed())),
+                web::resource("/")
+                    .route(web::get().to(HttpResponse::InternalServerError)),
             )
     })
     .bind("127.0.0.1:8080")?
