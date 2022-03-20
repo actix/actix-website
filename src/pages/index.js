d90f78adc9a7949c1183556dc3d466c66ba634ae
Santiago Fraire <santiwilly@gmail.com>
2022-03-20 10:56:18 +0100

    feat: add main example

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,7 @@ const Home = () => {
     <Layout description={siteConfig.tagline}>
       <Hero />
       <main className={styles.main}>
+        <MainExample />
         <Highlights />
         <Examples />
       </main>
@@ -65,6 +66,45 @@ const highlights = [
   },
 ];
 
+const Hero = () => {
+  const context = useDocusaurusContext();
+  const { siteConfig } = context;
+
+  return (
+    <header id="hero" className={clsx('hero', styles.banner)}>
+      <div className="container">
+        <img
+          src={useBaseUrl(`img/logo.png`)}
+          alt="Actix Logo"
+          className={styles.logo}
+        />
+
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className={clsx('hero__subtitle', styles.subtitle)}>
+          {siteConfig.tagline}
+        </p>
+
+        <div className={styles.buttons}>
+          <Link
+            className="button button--primary button--lg"
+            to={useBaseUrl('docs/')}
+          >
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+const MainExample = () => (
+  <div className={styles.main_example}>
+    <div className={styles.main_example__code}>
+      <CodeBlock example="main-example" section="main-example" />
+    </div>
+  </div>
+);
+
 const Highlights = () => {
   return (
     <>
@@ -95,37 +135,6 @@ const Highlights = () => {
         </div>
       </section>
     </>
-  );
-};
-
-const Hero = () => {
-  const context = useDocusaurusContext();
-  const { siteConfig } = context;
-
-  return (
-    <header id="hero" className={clsx('hero', styles.banner)}>
-      <div className="container">
-        <img
-          src={useBaseUrl(`img/logo.png`)}
-          alt="Actix Logo"
-          className={styles.logo}
-        />
-
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className={clsx('hero__subtitle', styles.subtitle)}>
-          {siteConfig.tagline}
-        </p>
-
-        <div className={styles.buttons}>
-          <Link
-            className="button button--primary button--lg"
-            to={useBaseUrl('docs/')}
-          >
-            Get Started
-          </Link>
-        </div>
-      </div>
-    </header>
   );
 };
 
