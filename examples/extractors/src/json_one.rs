9cdbbd3037700934eb1fe810ab56fd25dafb5686
Rob Ede <robjtede@icloud.com>
2022-07-30 18:18:40 +0100

    Update json_one.rs

--- examples/extractors/src/json_one.rs
+++ examples/extractors/src/json_one.rs
@@ -1,5 +1,5 @@
 // <json-one>
-use actix_web::{get, web, App, HttpServer, Result};
+use actix_web::{post, web, App, HttpServer, Result};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -8,15 +8,15 @@ struct Info {
 }
 
 /// deserialize `Info` from request's body
-#[post("/")]
-async fn index(info: web::Json<Info>) -> Result<String> {
+#[post("/submit")]
+async fn submit(info: web::Json<Info>) -> Result<String> {
     Ok(format!("Welcome {}!", info.username))
 }
 // </json-one>
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(submit))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
