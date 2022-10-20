a2145183ef8dfb6f61d8d6e7caa8fa3546cb5d82
Quentin Le Sceller <q.lesceller@gmail.com>
2022-10-20 11:56:19 -0400

    Fix integration test example

--- examples/testing/src/integration_one.rs
+++ examples/testing/src/integration_one.rs
@@ -24,7 +24,7 @@ mod tests {
 
     #[actix_web::test]
     async fn test_index_post() {
-        let app = test::init_service(App::new().route("/", web::get().to(index))).await;
+        let app = test::init_service(App::new().route("/", web::post().to(index))).await;
         let req = test::TestRequest::post().uri("/").to_request();
         let resp = test::call_service(&app, req).await;
         assert!(resp.status().is_client_error());
