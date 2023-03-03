04f6f0bd025d4dc271dde9793f6d7cd3eeed2d4a
Jonas Fassbender <jonas@fc-web.de>
2023-03-03 16:07:26 +0100

    Fixed keep-alive on response example (#316)
    * fixed keep-alive on response example
    
    * fixed dead code and unused variable errors

--- examples/server/src/keep_alive_tp.rs
+++ examples/server/src/keep_alive_tp.rs
@@ -1,11 +1,17 @@
+#![allow(dead_code)]
+
 // <example>
 use actix_web::{http, HttpRequest, HttpResponse};
 
-async fn index(req: HttpRequest) -> HttpResponse {
-    HttpResponse::Ok()
-        .connection_type(http::ConnectionType::Close) // <- Close connection
-        .force_close() // <- Alternative method
-        .finish()
+async fn index(_req: HttpRequest) -> HttpResponse {
+    let mut resp = HttpResponse::Ok()
+        .force_close() // <- Close connection on HttpResponseBuilder
+        .finish();
+
+    // Alternatively close connection on the HttpResponse struct
+    resp.head_mut().set_connection_type(http::ConnectionType::Close);
+
+    resp
 }
 // </example>
 
