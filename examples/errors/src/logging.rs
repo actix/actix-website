7d423b8b076ca1f5a6d75cc5eafd4d62bdd950d0
Yuki Okushi <huyuumi.dev@gmail.com>
2026-02-01 12:37:19 +0900

    feat: upgrade to edition 2024

--- examples/errors/src/logging.rs
+++ examples/errors/src/logging.rs
@@ -22,8 +22,10 @@ async fn index() -> Result<&'static str, MyError> {
 #[rustfmt::skip]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "info");
-    std::env::set_var("RUST_BACKTRACE", "1");
+    unsafe {
+        std::env::set_var("RUST_LOG", "info");
+        std::env::set_var("RUST_BACKTRACE", "1");
+    }
     env_logger::init();
 
     HttpServer::new(|| {
